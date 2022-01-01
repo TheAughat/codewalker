a25db1e9ae067f95fffb1f2a29894ad4564dfeeb
Daniel Rodrigues <rcdan22@gmail.com>
2022-01-01 13:25:10 +0000

    changes to index.html and server status coloring added

--- src/codewalker-components/ServerConnect.js
+++ src/codewalker-components/ServerConnect.js
@@ -14,20 +14,23 @@ const ServerConnect = () => {
         if (status === 'online') {
             setBtnStatus('Connect');
             setStatus('offline');
+            document.getElementsByClassName('status-text')[0].style = 'color:#cc0000;';
             await fetch('http://localhost:8080/stop-server');
         }
         else if (status === 'offline') {
+            document.getElementsByClassName('status-text')[0].style = 'color:#cc0000;';
             const res = await fetch('http://localhost:8080/codewalker-check').then(resp => resp.json());
             if (res.result === 'cw server up and running') {
                 setBtnStatus('Stop Server');
                 setStatus('online');
+                document.getElementsByClassName('status-text')[0].style = 'color:#00802b;';
             }
         }
     }
 
     return (
         <div className="server-connect">
-            <p>Server status: {status}</p>
+            <p className='status-text'>Server status: {status}</p>
             <button className="btn connect-btn" onClick={async () => {
                 makeConnection();
             }}>{btnStatus}</button>
